54af525715ae64819f80a9b267d13f33dc306bb4
NayZ <55506519+nayzflux@users.noreply.github.com.>
2023-11-25 17:04:14 +0100

    Adding animals route ✨

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,52 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+interface Database {
+    animals: Animal[]
+}
+
+interface Animal {
+    id: number;
+    name: string;
+}
+
+const db: Database = {
+    animals: [
+        {
+            id: 1,
+            name: 'Lion 🦁'
+        },
+        {
+            id: 2,
+            name: 'Lézard 🦎'
+        },
+        {
+            id: 3,
+            name: 'Tortue 🐢'
+        },
+        {
+            id: 4,
+            name: 'Chien 🐕'
+        },
+        {
+            id: 5,
+            name: 'Poule 🐔'
+        },
+    ]
+}
+
 app.get('/', async (req, res) => {
     res.status(200).json({
         message: 'Hello World! ✨',
         version: 2
+    });
+});
+
+app.get('/animals', async (req, res) => {
+    const animals = db.animals;
+
+    res.status(200).json({
+        animals
     });
 });
 
